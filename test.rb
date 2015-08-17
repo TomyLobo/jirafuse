a9d5c5fa9e283f199ca569e94fea317902d01404
TomyLobo <tomylobo@nurfuerspam.de>
2015-08-17 22:33:06 +0000

    routing works

--- test.rb
+++ test.rb
@@ -1,32 +1,57 @@
 #!/usr/bin/env ruby
 
-$routes = {}
+$routes = []
+
+class Route
+    def initialize(pattern, hash)
+        @hash = hash
+        @keys = []
+
+        # scan pattern for /:.../ (TODO: Regexp.escape the rest?)
+        pattern = pattern.gsub(/\/:([^\/]+)\//) {
+            # store matches in order
+            @keys << $1.to_sym
+
+            # replace by /([^/]*)/
+            '/([^/]*)/'
+        }
+        @regex = /^#{pattern}$/
+    end
+
+    def match(path)
+        # match path
+        match = @regex.match(path)
+        return nil unless match
+        # assign to params dict by order
+        Hash[@keys.zip(match.captures)]
+    end
+
+    def dispatch(path)
+        params = match(path)
+        return false unless params
+
+        send(@hash[:to], params)
+        return true
+    end
+end
 
 
 def get(pattern, hash)
-    $routes[pattern] = hash
-    puts pattern
-    puts hash
-    send(hash[:to])
+    $routes << Route.new(pattern, hash)
 end
 
-def testfunc
-    puts 'from testfunc'
+def route_path(path)
+    $routes.each do |route|
+        success = route.dispatch(path)
+        return if success
+    end
 end
 
+def testfunc(params)
+    puts "from testfunc: #{params}"
+end
 
 get '/issues', to: :testfunc
 get '/issues/:id/comments', to: :testfunc
 
-
-mypath = '/issues/JFS-1/comments'
-
-$routes.each do |pattern, hash|
-    # TODO:
-    # scan pattern for /: (Regexp.escape the rest?)
-    # replace by ([^/]*)
-    # store order
-    # match mypath
-    # assign to params dict by order
-    #pattern.matches
-end
+route_path('/issues/JFS-1/comments')
