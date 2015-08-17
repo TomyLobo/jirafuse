ada3a6ec34b2da234900edd11d6fd1b56e45f624
TomyLobo <tomylobo@nurfuerspam.de>
2015-08-17 22:33:07 +0000

    Changed .../comments to return a json and added .../comment_bodies with the previous behaviour.

--- jiradir.rb
+++ jiradir.rb
@@ -24,6 +24,11 @@ class JiraDir < RoutedDir
         @jira.get("/issue/#{params[:issue]}/comment/#{params[:comment]}")
     end
 
+    def read_issue_comment_json(params)
+        # return the HTTP body
+        read_issue_comment(params).body
+    end
+
     def read_issue_comment_body(params)
         # return the value for the "body" key in the JSON object in the HTTP body
         read_issue_comment(params)['body']
@@ -33,7 +38,9 @@ class JiraDir < RoutedDir
      route_add :list, '/projects', to: :list_projects
       route_add :list, '/projects/:project', to: [ 'issues' ]
        route_add :list, '/projects/:project/issues', to: :list_project_issues
-        route_add :list, '/projects/:project/issues/:issue', to: [ 'comments' ]
+        route_add :list, '/projects/:project/issues/:issue', to: [ 'comments', 'comment_bodies' ]
          route_add :list, '/projects/:project/issues/:issue/comments', to: :list_issue_comments
-          route_add :read, '/projects/:project/issues/:issue/comments/:comment', to: :read_issue_comment_body
+          route_add :read, '/projects/:project/issues/:issue/comments/:comment', to: :read_issue_comment_json
+         route_add :list, '/projects/:project/issues/:issue/comment_bodies', to: :list_issue_comments
+          route_add :read, '/projects/:project/issues/:issue/comment_bodies/:comment', to: :read_issue_comment_body
 end
